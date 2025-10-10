ed2aa3ce81c4e0a60309af53a5d8c7b09bdf520a
Dylan Weber <dylanweber06@gmail.com>
2025-10-10 01:01:24 -0400

    Added git aliases and nomacs floating windowrule

--- zsh/.config/zsh/zsh_aliases.zsh
+++ zsh/.config/zsh/zsh_aliases.zsh
@@ -4,3 +4,9 @@ if command -v eza &>/dev/null; then
     alias tree='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first --tree'
 fi
 # =====================================
+
+# ============ Git aliases ============
+alias gaa='git add --all'
+alias gc='git commit'
+alias gp='git push'
+# =====================================
