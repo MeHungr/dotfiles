0b190f15a3ee91ab92497a0319ddb7fb756a07f9
Dylan Weber <dylanweber06@gmail.com>
2026-04-08 18:16:19 -0400

    Changed configs to config in treesitter.lua (hate updates bro)

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter'}, 
+    dependencies = { 'nvim-treesitter/nvim-treesitter'},
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
