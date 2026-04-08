947569917de70597e02ef97f1b771d2685e2c2c4
Dylan Weber <dylanweber06@gmail.com>
2026-04-08 18:41:45 -0400

    Now using main branch of treesitter instead of outdated master branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,18 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            local config = require("nvim-treesitter.config")
-            config.setup({
-                ensure_installed = { "lua", "java", "go", "python", "c" },
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
+            -- Install parsers
+            require("nvim-treesitter").install({ "lua", "java", "go", "python", "c" }):wait(300000)
+
+            -- Enable highlighting per filetype
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "lua", "java", "go", "python", "c" },
+                callback = function()
+                    vim.treesitter.start()
+                end,
             })
         end,
     },
